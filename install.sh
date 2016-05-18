8f8354c1ce49fb1d0761f4b097877145170d6252
xiaofangECL <celine.chinoise@gmail.com>
2016-05-18 11:00:26 +0200

    monokai color

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+echo "---- remove original vim configureation------"
+sudo rm -r ~/.vim/  
+rm ~/.vimrc 
 echo "-----------install xiaofang wang's vim configuration------------"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "create .vimrc file"
-wget https://github.com/xiaofangECL/vim_configure/blob/master/vimrc
+cp .vimrc ~/.vimrc
+#wget https://github.com/xiaofangECL/vim_configure/blob/master/vimrc
 echo "install vim plugins.."
 cd ~/.vim/
 git init
-git submodule add git://github.com/Lokaltog/vim-easymotion.git bundle/easymotion
+git  https://github.com/sickill/vim-monokai.git 
+cp -r vim-monokai/colors ~/.vim/   
+#git submodule add git://github.com/Lokaltog/vim-easymotion.git bundle/easymotion
 
